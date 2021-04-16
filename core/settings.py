4d8755bc2af9a678ca0790ab047c65f4ed379d7b
srayas <srayas.parolathodi@tatacommunications.com>
2021-04-16 20:46:22 +0530

    Api connections is done

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 ASGI_APPLICATION ="core.routing.application"
+CHANNEL_LAYERS = {
+        "default":{
+                "BACKEND":"channels.layers.InMemoryChannelLayer"
+        }
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
